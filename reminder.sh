11439d6e9a7e2c9be9175bb3b7bb196e9c0b7382
ZW <zhaowuwei2020@foxmail.com>
2021-05-21 15:13:20 +0800

    [ZWW]: Set action.

---
+++ reminder.sh
@@ -0,0 +1,10 @@
+curl 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=4b42ef08-d001-4aec-8cc3-344f202634e8' \
+   -H 'Content-Type: application/json' \
+   -d '
+   {
+        "msgtype": "text",
+        "text": {
+            "content": "【Code Review】17:00；981 7348 4620",
+            "mentioned_list":["@all"]
+        }
+   }'
